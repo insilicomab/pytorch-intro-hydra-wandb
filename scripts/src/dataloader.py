26fb6fd9d9675a235074ef27710c624a26df9bb0
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2022-08-31 20:27:31 +0900

    Update dataloader.py

--- scripts/src/dataloader.py
+++ scripts/src/dataloader.py
@@ -3,7 +3,8 @@ from torch.utils.data import DataLoader
 
 def make_dataloader(tr_dataset, val_dataset, tr_batch_size, val_batch_size):
     dataloader = {
-    'train': DataLoader(tr_dataset, batch_size=tr_batch_size, shuffle=True),
-    'val': DataLoader(val_dataset, batch_size=val_batch_size, shuffle=False)
+        'train': DataLoader(tr_dataset, batch_size=tr_batch_size, shuffle=True),
+        'val': DataLoader(val_dataset, batch_size=val_batch_size, shuffle=False)
     }
+    
     return dataloader
